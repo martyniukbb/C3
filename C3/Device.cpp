feaadabb1cebd44750505dd31b0c978ddb3debb3
martyniukbb <martyniukBB@gmail.com>
2018-06-20 17:32:01 +0300

    'commit2'

--- C3/Device.cpp
+++ C3/Device.cpp
@@ -30,6 +30,7 @@ void Device::pathRestoration(string inputFile, string outputFile) {
 
 		vector<pack_input> lastReadPack;
 		vector<pack_output> lastOutputPack;
+		vector<omega> lastOmegaPack;
 
 		for (int j = 0; (j < numberPack) && (i < count); j++, i++) {
 			pack_input readBuffer;
@@ -37,8 +38,25 @@ void Device::pathRestoration(string inputFile, string outputFile) {
 			lastReadPack.push_back(readBuffer);
 		}
 
+		for (int i = 0; i < lastReadPack.size()-1; i++) {
+			omega pushBuffer;
+			
+			double deltaTime = lastReadPack[i + 1].t - lastReadPack[i].t;
+
+			pushBuffer.omega_x = (lastReadPack[i + 1].fi_x - lastReadPack[i].fi_x)
+				/ deltaTime;
+			pushBuffer.omega_y = (lastReadPack[i + 1].fi_y - lastReadPack[i].fi_y)
+				/ deltaTime;
+			pushBuffer.omega_z = (lastReadPack[i + 1].fi_z - lastReadPack[i].fi_z)
+				/ deltaTime;
+
+			lastOmegaPack.push_back(pushBuffer);
+		}
+
+		lastOmegaPack = smoothing(lastOmegaPack);
+
 		///////////////
-		algorithmPathRestoration(lastReadPack, lastOutputPack, state, state_input);
+		algorithmPathRestoration(lastReadPack, lastOutputPack, lastOmegaPack, state, state_input);
 		///////////////
 
 		for (int j = 0; j < int(lastOutputPack.size()); j++) {
@@ -49,7 +67,7 @@ void Device::pathRestoration(string inputFile, string outputFile) {
 	file_out.close();
 }
 
-void Device::algorithmPathRestoration(vector<pack_input> &input, vector<pack_output> &output, pack_output &state, pack_input &state_input) {
+void Device::algorithmPathRestoration(vector<pack_input> &input, vector<pack_output> &output, vector<omega> omegaPack, pack_output &state, pack_input &state_input) {
 	
 	for (int i = 0; i < input.size(); i++) {
 		input[i].accel_z -= g;
@@ -67,6 +85,11 @@ void Device::algorithmPathRestoration(vector<pack_input> &input, vector<pack_out
 
 		state.v_y += delta_time * input[i].accel_y;
 		state.v_z += delta_time * input[i].accel_z;
+
+		//////////////
+		if(i < input.size() - 1)
+			subtractionCentrifugalForce(state, input[i], omegaPack[i], delta_time);
+		//////////////
 
 		backRotate(input[i], state_input, state);
 
@@ -110,7 +133,7 @@ void Device::backRotate(pack_input &inputNext, pack_input &inputFirst, pack_outp
 		gamma = inputFirst.fi_z;
 
 	//в неподвижной системе координат
-	double v_x = output.v_x * (cos(alfa)*cos(gamma) - sin(alfa)*cos(beta)*sin(gamma))
+	/*double v_x = output.v_x * (cos(alfa)*cos(gamma) - sin(alfa)*cos(beta)*sin(gamma))
 		+ output.v_y * (sin(alfa) * cos(gamma) + cos(alfa) * cos(beta) * sin(gamma))
 		+ output.v_z * (sin(beta) * sin(gamma));
 
@@ -120,7 +143,19 @@ void Device::backRotate(pack_input &inputNext, pack_input &inputFirst, pack_outp
 
 	double v_z = output.v_x * (sin(alfa)*sin(beta))
 		+ output.v_y * (-cos(alfa)*sin(beta))
-		+ output.v_z * cos(beta);
+		+ output.v_z * cos(beta);*/
+
+	double v_x = output.v_x * (cos(beta)*cos(gamma))
+		+ output.v_y * (cos(alfa) * sin(gamma) + cos(gamma)*sin(alfa)*sin(beta))
+		+ output.v_z * (sin(alfa)*sin(gamma) - cos(alfa)*cos(gamma)*sin(beta));
+
+	double v_y = output.v_x * (-1 * cos(beta)*sin(gamma))
+		+ output.v_y * (cos(alfa)*cos(gamma) - sin(alfa)*sin(beta)*sin(gamma))
+		+ output.v_z * (cos(gamma)*sin(alfa) + cos(alfa)*sin(beta)*sin(gamma));
+
+	double v_z = output.v_x * (sin(beta))
+		+ output.v_y * (-1 * cos(beta)*sin(alfa))
+		+ output.v_z * (cos(alfa)*cos(beta));
 
 	output.t = inputNext.t;
 	output.x += delta_time * v_x;
@@ -146,6 +181,35 @@ vector<pack_input> Device::smoothing(vector<pack_input> &input) {
 	result.push_back(input[input.size() - 1]);
 
 	return result;
+}
+
+vector<omega> Device::smoothing(vector<omega> &input) {
+	vector<omega> result;
+
+	for (int i = 0; i < input.size() - 1; i++) {
+
+		omega push;
+
+		push = input[i];
+
+		push.omega_x = (input[i].omega_x + input[i + 1].omega_x) / 2.0;
+		push.omega_y = (input[i].omega_y + input[i + 1].omega_y) / 2.0;
+		push.omega_z = (input[i].omega_z + input[i + 1].omega_z) / 2.0;
+
+		result.push_back(push);
+	}
+	result.push_back(input[input.size() - 1]);
+
+	return result;
+}
+
+void Device::subtractionCentrifugalForce(pack_output &state, pack_input &input, omega stateOmega, double deltaTime) {
+	double vx = stateOmega.omega_y * state.v_z - stateOmega.omega_z * state.v_y,
+		vy = -1 * (stateOmega.omega_x * state.v_z - stateOmega.omega_z * state.v_x),
+		vz = stateOmega.omega_x * state.v_y - stateOmega.omega_y * state.v_x;
+	state.v_x -= deltaTime * vx;
+	state.v_y -= deltaTime * vy;
+	state.v_z -= deltaTime * vz;
 }
 
 Device::~Device()
--- C3/Device.h
+++ C3/Device.h
@@ -6,6 +6,17 @@
 #define g 9.81
 #define numberPack 10
 
+typedef struct omega {
+	omega() {
+		omega_x = 0.0;
+		omega_y = 0.0;
+		omega_z = 0.0;
+	}
+	double omega_x,
+		omega_y,
+		omega_z;
+} omega;
+
 typedef struct pack_input {
 	pack_input() {
 		t = 0.0;
@@ -53,11 +64,13 @@ class Device
 public:
 	Device();
 	static void pathRestoration(string inputFile, string outputFile);
-	static void algorithmPathRestoration(vector<pack_input> &input, vector<pack_output> &output, pack_output &state, pack_input &state_input);
+	static void algorithmPathRestoration(vector<pack_input> &input, vector<pack_output> &output, vector<omega> omegaPack, pack_output &state, pack_input &state_input);
 	static void writePackToFile(ofstream &out, pack_output &packOut);
 	static void readPackOfFile(ifstream &inp, pack_input &packInp);
 	static void backRotate(pack_input &inputNext, pack_input &inputFirst, pack_output &output);
 	static vector<pack_input> smoothing(vector<pack_input> &input);
+	static vector<omega> smoothing(vector<omega> &input);
+	static void subtractionCentrifugalForce(pack_output &state, pack_input &input, omega stateOmega, double deltaTime);
 	~Device();
 };
 
